e8dc6aeb1c412fbfab1057672e817e9fe78de8b4
pranjanpr <pratyush.ranjan911@gmail.com>
2020-07-05 23:35:18 +0530

    Updates

---
+++ precache-manifest.08f7eeb186007ecb60767d1b44931c64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "514d53e0d5ff5715b52a534aa6f55fee",
+    "url": "/pratyush/index.html"
+  },
+  {
+    "revision": "7aaaf45bba2b20437e18",
+    "url": "/pratyush/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/pratyush/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pratyush/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aaaf45bba2b20437e18",
+    "url": "/pratyush/static/js/main.e1115ade.chunk.js"
+  },
+  {
+    "revision": "2af4845e9a598c1f76ed",
+    "url": "/pratyush/static/js/runtime-main.896e6180.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pratyush/static/media/logo.5d5d9eef.svg"
+  }
+]);

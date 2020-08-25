075fe44c7af8c48430702155832d00cf001618d4
pranjanpr <pratyush.ranjan911@gmail.com>
2020-08-25 22:42:52 +0530

    modified

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import '../App.css';
+
 import {BrowserRouter as Router ,Link} from 'react-router-dom';
 import Main from './main';
 import Typical from 'react-typical';
 import {useSpring,animated} from 'react-spring';
+import { faFacebook ,faLinkedin,faTwitter,faInstagram,faLinkedinIn,faFacebookF} from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Blogs=()=>{
     const springs = useSpring({from:{ marginTop: -1000}, to:{ marginTop: 0 }})
     const spring2=useSpring({from:{marginTop:-1000},to:{marginTop:0}})
+    const spring3=useSpring({from:{marginLeft:-3000},to:{marginLeft:0}})
+    const spring4=useSpring({from:{opacity:0},to:{opacity:1},config: {
+      duration: 1000,
+    },})
 return(
 
   <div className="App" >
@@ -25,24 +32,27 @@ return(
 
     </animated.header>
 
-    <div className="main-wrapper">
-
-      <nav className="main-navv" role="navigation">
-        <ul classNav="main-menu">
+    <div className="main-wrapper" >
+    <animated.div className="imagediv" style={spring4}>
+      <a href="#"><img className="imagee" src={require('./side.png')} alt="" /></a>
+    </animated.div>
+      <animated.nav className="main-navv" role="navigation" style={spring3}>
+        <ul className="main-menu">
           <li><a href="#section1">About Me</a></li>
-          <li><a href="#section2">What I’m good at</a></li>
+          
           <li><a href="#section3">My Work</a></li>
+          <li><a href="#section4">CP Handles</a></li>
           <li><a href="#section4">Contact Me</a></li>
+          
         </ul>
-      </nav>
+      </animated.nav>
 
     <div className="social-network">
       <ul className="soial-icons">
-        <li><a href="https://fb.com/templatemo"><i className="fa fa-facebook"></i></a></li>
-        <li><a href="#"><i className="fa fa-twitter"></i></a></li>
-        <li><a href="#"><i className="fa fa-linkedin"></i></a></li>
-        <li><a href="#"><i className="fa fa-dribbble"></i></a></li>
-        <li><a href="#"><i className="fa fa-rss"></i></a></li>
+        <li><a><FontAwesomeIcon  icon={faFacebookF} /></a></li>
+        <li><a><FontAwesomeIcon  icon={faLinkedinIn} /></a></li>
+        <li><a><FontAwesomeIcon  icon={faTwitter} /></a></li>
+        <li><a><FontAwesomeIcon  icon={faInstagram} /></a></li>
       </ul>
     </div>
 
@@ -87,6 +97,9 @@ return(
       </div>
       <div className="fixed-bg bg-3">
         <h1>Fixed Background scrolling</h1>
+      </div>
+      <div className="scroll-bg">
+      <p>Copyright© 2020 Pratyush Ranjan</p>
       </div>
     </div> 
     

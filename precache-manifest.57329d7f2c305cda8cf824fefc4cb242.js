ec7b1995a88d6169591e1a31813b496496881dac
pranjanpr <pratyush.ranjan911@gmail.com>
2021-08-23 01:29:35 +0530

    Updates

---
+++ precache-manifest.57329d7f2c305cda8cf824fefc4cb242.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9186b790e931a99499aa640bae1b6b03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ea3c8e06745e7add72b",
+    "url": "/static/css/main.f74642fc.chunk.css"
+  },
+  {
+    "revision": "94661a1449e4d84ca2b7",
+    "url": "/static/js/2.7e6823bd.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.7e6823bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea3c8e06745e7add72b",
+    "url": "/static/js/main.1eebabca.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "3b93c75036caab65ff7a0345e82af55b",
+    "url": "/static/media/backg.3b93c750.png"
+  },
+  {
+    "revision": "1804f36d145b57218c47baebd69122ea",
+    "url": "/static/media/dhiraj.1804f36d.jpg"
+  },
+  {
+    "revision": "0b97115e6af71d76f7c733a710041fac",
+    "url": "/static/media/iitk.0b97115e.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "02215aa106f946a532606acdd99276fa",
+    "url": "/static/media/side.02215aa1.png"
+  },
+  {
+    "revision": "1ce8c3aef005c4e23ac9e6e47ee37872",
+    "url": "/static/media/till_openprinting.1ce8c3ae.jpg"
+  }
+]);
